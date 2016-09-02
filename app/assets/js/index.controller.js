6e52b30413853172c634975d3e2d598d8602b585
abhinav <abhi.aec89@gmail.com>
2016-09-02 18:59:01 +0530

    Added Carousel Categories

--- app/assets/js/index.controller.js
+++ app/assets/js/index.controller.js
@@ -5,6 +5,33 @@
     "use strict";
     angular.module("digiapt").
     controller("rootController", ["$scope", function ($scope) {
+        $scope.carousel = [];
+        $scope.optionThree = ["car1.jpg","car2.jpg","car3.jpg"];
+        $scope.optionTwo = ["hack1.jpg","hack2.jpg","hack3.jpg"];
+        $scope.optionOne = ["phone1.jpg","phone2.jpg","phone3.jpg"];
+        $scope.carousel = $scope.optionOne;
+        $scope.formData = "phone";
+        $scope.data = {
+            "Question": "What do you like most??",
+            "answers" : ["phone","car","hack"]
+        }
+
+        $scope.updateCarousel = function (formData) {
+            $scope.carousel = [];
+            switch(formData){
+                case "phone":
+                    $scope.carousel = $scope.optionOne;
+                    break;
+                case "car":
+                    $scope.carousel = $scope.optionThree;
+                    break;
+                case "hacking":
+                    $scope.carousel = $scope.optionTwo;
+                    break;
+                default:
+                    break;
+            }
+        }
 
     }])
         .controller("resultController",["$scope", "$stateParams",function ($scope,$stateParams) {
