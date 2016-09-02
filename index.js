b80c0d583a00531c4c1f56cc152279450bcb9997
abhinav <abhi.aec89@gmail.com>
2016-09-02 17:45:02 +0530

    updated ip and port

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
 var fs = require('fs'),
     http = require('http'),
     https = require('https'),
-    express = require('express');
+    express = require('express'),
+    process = require('process');
 
 var app = express();
 
 app.use(express.static('./'));
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-var server = http.createServer(app).listen(3000, function(){
-    console.log("SSL Express server listening on port " + 3000);
+var server = http.createServer(app).listen(server_port, server_host, function(){
+    console.log("SSL Express server listening on port " + server_port);
 });
